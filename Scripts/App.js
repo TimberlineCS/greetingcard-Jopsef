ef8e64fd0d816e40fc62c634b8212d16612635ce
Jopsef <banana.jose@yahoo.com>
2023-12-12 15:06:40 -0700

    just saving the work, not done

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,6 +1,61 @@
-//Your name
-//Period
-//Date
+//joseph
+//7
+//11/12/23
+
+document.body.style.backgroundImage = "url('Images/moon.gif')";
+
+var wall = document.createElement("div");
+wall.style.width = "660px";
+wall.style.height = "350px";
+wall.style.border = "black solid 5px";
+wall.style.backgroundColor = "white"
+wall.style.borderRadius = "0"
+wall.style.position = ("absolute");
+wall.style.left = ("0px");
+wall.style.bottom = ("0px");
+document.body.append(wall);
+
+var II = document.createElement("div");
+var i = document.createElement("img");
+i.src = "https://en-wiki.metin2.gameforge.com/images/b/ba/Hungry_Alpha_Wolf.png";
+document.body.append(i);
+document.body.append(II);
+i.style.position = ("absolute");
+i.style.left = ("150px");
+i.style.bottom = ("0px");
+
+var CAGE = document.createElement("div");
+var cage = document.createElement("img");
+cage.src = "https://static.vecteezy.com/system/resources/thumbnails/008/505/771/small/wire-fence-illustration-png.png";
+document.body.append(cage);
+document.body.append(CAGE);
+cage.style.position = ("absolute");
+cage.style.left = ("150px");
+cage.style.bottom = ("0px");
+
+
+var box = document.createElement("div");
+box.style.width = "50px";
+box.style.height = "250px";
+box.style.border = "black solid 5px";
+box.style.backgroundColor = "#5C4033"
+box.style.borderRadius = "0"
+box.style.position = ("absolute");
+box.style.left = ("100px");
+box.style.bottom = ("0px");
+document.body.append(box);
+
+var box2 = document.createElement("div");
+box2.style.width = "50px";
+box2.style.height = "250px";
+box2.style.border = "black solid 5px";
+box2.style.backgroundColor = "#5C4033"
+box2.style.borderRadius = "0"
+box2.style.position = ("absolute");
+box2.style.left = ("498px");
+box2.style.bottom = ("0px");
+document.body.append(box2);
+
 
 /*Provide a description about what this program does here*/
 
